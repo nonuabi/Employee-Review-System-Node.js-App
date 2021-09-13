b9808979de755553b39f1448997b04002728efb4
nonuabi <nonuabi@gmail.com>
2021-09-13 18:55:20 +0530

    add passport local Strategy

--- config/password-local-strategy.js
+++ config/password-local-strategy.js
@@ -1,8 +1,44 @@
+const User = require("../model/user");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
+// Authentication using passport
 passport.use(
-  new LocalStrategy({
-    usernameField: "email",
-  })
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    function (email, password, done) {
+      // find the user and establish the identiy
+      User.findOne({ email }, function (err, user) {
+        if (err) {
+          console.log("Error in finding the user : passport : ", err);
+          return done(err);
+        }
+        if (!user || user.password !== password) {
+          console.log("Invalid username/password");
+          done(null, false);
+        }
+        done(null, user);
+      });
+    }
+  )
 );
+
+// serializing the user to decide which key is to be kept in the cookies
+passport.serialization(function (user, done) {
+  done(null, user.id);
+});
+
+// deserializing the user from the key in the cookies
+passport.deserializeUser(function (id, done) {
+  User.findById({ id }, function (err, user) {
+    if (err) {
+      console.log("Error in finding User : passport : ", err);
+      return;
+    }
+    return done(null, user);
+  });
+});
+
+module.exports = passport;
