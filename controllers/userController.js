84d76f39d6c49580bd2ec9253d3598e7d92c3e4e
nonuabi <nonuabi@gmail.com>
2021-09-13 21:24:56 +0530

    add login , signup routers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,33 @@
+const User = require("../model/user");
+
 module.exports.home = function (req, res) {
   return res.render("home");
+};
+
+module.exports.login = function (req, res) {
+  return res.render("login");
+};
+
+module.exports.signup = function (req, res) {
+  return res.render("signup");
+};
+
+// create new user
+module.exports.create = async function (req, res) {
+  console.log("create user");
+  try {
+    if (req.body.passpord !== req.body.confirmPassword) {
+      let check_user = await User.findOne({ email });
+      console.log(" ::: ", check_user);
+      return res.render("signup");
+    } else {
+      console.log("Password && Confirm Password do not match");
+      return res.render("signup");
+    }
+  } catch (err) {
+    console.log("Error while creating new user :: ", err);
+    return res.render("home");
+  }
 };
 
 // Sign In and create a session for the user
--- router/index.js
+++ router/index.js
@@ -4,12 +4,15 @@ const passport = require("passport");
 const userController = require("../controllers/userController");
 
 router.get("/", userController.home);
+router.get("/login", userController.login);
+router.get("/signup", userController.signup);
+router.post("/user/create-user", userController.create);
 // use passport as a middleware to authenticate
-router.post(
-  "/create-session",
-  passport.authenticate("local", {
-    failureRedirect: "/",
-  }),
-  userController.createSession
-);
+// router.post(
+//   "/create-session",
+//   passport.authenticate("local", {
+//     failureRedirect: "/",
+//   }),
+//   userController.createSession
+// );
 module.exports = router;
